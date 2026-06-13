b5103fd9ea643a3bebfcb61d7eeb29d0a134eb4d
Sahadat Hossen <sahadatjh@gmail.com>
2026-06-13 18:05:32 +0600

    Header modify whole project

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const navPlaceholder = document.getElementById('navbar-placeholder');
     const headerPlaceholder = document.getElementById('header-placeholder');
     const footerPlaceholder = document.getElementById('footer-placeholder');
-
-    if (navPlaceholder) {
-        fetch('/navbar.html')
-            .then(res => res.text())
-            .then(data => {
-                navPlaceholder.innerHTML = data;
-                // Execute scripts in the loaded HTML
-                const scripts = navPlaceholder.querySelectorAll('script');
-                scripts.forEach(oldScript => {
-                    const newScript = document.createElement('script');
-                    Array.from(oldScript.attributes).forEach(attr => newScript.setAttribute(attr.name, attr.value));
-                    newScript.appendChild(document.createTextNode(oldScript.innerHTML));
-                    oldScript.parentNode.replaceChild(newScript, oldScript);
-                });
-            })
-            .catch(err => console.error('Failed to load navbar:', err));
-    }
 
     if (headerPlaceholder) {
         fetch('/header.html')
